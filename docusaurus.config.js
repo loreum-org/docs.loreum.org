3955f04367accb58b5626954d9b3a3aa6401299c
Chad Lynch <chyad@hotmail.com>
2023-05-17 15:54:07 -0400

    fix: edit page url

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -41,7 +41,7 @@ const config = {
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
           editUrl:
-            'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
+            'https://github.com/loreum-org/docs.loreum.org/',
         },
         theme: {
           customCss: require.resolve('./src/css/custom.css'),
